468c90cda88036e9648b7d48ac7a1b0229cb6e17
Oliver Ryan <oliverryan482@gmail.com>
2024-07-09 12:35:00 -0400

    added all the metrics

--- weather_station_server/server.py
+++ weather_station_server/server.py
@@ -22,6 +22,24 @@ REQUEST_LATENCY = Histogram(
 GAUGE_DEFS = [
     ('station_outdoor_temp_f', 'Outdoor Temperature, F', 'tempf'),
     ('station_outdoor_windspeed_mph', 'Wind Speed, MPH', 'windspeedmph'),
+    ('station_outdoor_humidity_pct', 'Outdoor Humidity, %', 'humidity'),
+    ('station_outdoor_windgustmph_mph', 'Wind Gust Speed, MPH', 'windgustmph'),
+    ('station_outdoor_maxdailygust_mph', 'Max Daily Wind Gust, MPH', 'maxdailygust'),
+    ('station_outdoor_winddir_deg', 'Wind Direction, Degrees', 'winddir'),
+    ('station_outdoor_uv_num', 'UV Index', 'uv'),
+    ('station_outdoor_solarradiation_num', 'Solar Radiation, W/m²', 'solarradiation'),
+    ('station_outdoor_hourlyrainin_in', 'Hourly Rainfall, in', 'hourlyrainin'),
+    ('station_outdoor_eventrainin_in', 'Event Rainfall, in', 'eventrainin'),
+    ('station_outdoor_dailyrainin_in', 'Daily Rainfall, in', 'dailyrainin'),
+    ('station_outdoor_weeklyrainin_in', 'Weekly Rainfall, in', 'weeklyrainin'),
+    ('station_outdoor_monthlyrainin_in', 'Monthly Rainfall, in', 'monthlyrainin'),
+    ('station_outdoor_yearlyrainin_in', 'Yearly Rainfall, in', 'yearlyrainin'),
+    ('station_outdoor_totalrainin_in', 'Total Rainfall, in', 'totalrainin'),
+    ('station_outdoor_battout_num', 'Outdoor Battery Status', 'battout'),
+    ('station_indoor_tempinf_f', 'Indoor Temperature, F', 'tempinf'),
+    ('station_indoor_humidityin_pct', 'Indoor Humidity, %', 'humidityin'),
+    ('station_outdoor_baromrelin_in', 'Relative Barometric Pressure, inHg', 'baromrelin'),
+    ('station_outdoor_baromabsin_in', 'Absolute Barometric Pressure, inHg', 'baromabsin'),
 ]
 
 GAUGES = {
